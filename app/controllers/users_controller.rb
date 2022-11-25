ae813f7df8174c17e15148d44416bb26faf982a0
littlephillips <phillipkinuthia72@gmail.com>
2022-11-25 13:51:48 +0300

    rails display associated data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        if user
+            render json: user, include: :items
+        else 
+            render json: { error: "User not found"}, status: :not_found
+        end
+    end
+
+
 end
